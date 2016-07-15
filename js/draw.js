4466884de9ae693a943b24e224c946c5975e95fa
Shenghao Huang <davis.huangsh@gmail.com>
2016-07-15 08:01:09 -0700

    Replaced d3 with fabric.js, use the free drawing mode in fabric.js to power the pencil

---
+++ js/draw.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var chart = new fabric.Canvas('c', {
+    containerClass: 'container well'
+   // width: '100%'//,
+    //border: '2px solid #000000'
+  });
+
+  chart.isDrawingMode = true;
+  //chart.freeDrawingBrush.color="#000000";
+  //chart.freeDrawingBrush.width=30;
+});
