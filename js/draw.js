b9826c243c2fc04f62a518a6443eaaed339704be
Shenghao Huang <davis.huangsh@gmail.com>
2016-07-18 21:50:34 -0700

    Open a file, drag and drop is now working

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var chart = new fabric.Canvas('c', {
-    containerClass: 'container well'
+    containerClass: 'container well drop-zone'
    // width: '100%'//,
     //border: '2px solid #000000'
   });
@@ -8,4 +8,50 @@ $(document).ready(function() {
   chart.isDrawingMode = true;
   //chart.freeDrawingBrush.color="#000000";
   //chart.freeDrawingBrush.width=30;
+
+
+  // Setup the dnd listeners.
+  var dropZone = document.getElementsByClassName('drop-zone')[0];
+
+  function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = (evt.dataTransfer|| evt.target).files; // FileList object.
+
+    // files is a FileList of File objects. List some properties.
+
+
+    for (var i = 0, f; f = files[i]; i++) {
+      var reader = new FileReader();
+      reader.onload = function(evt) {
+        fabric.Image.fromURL(evt.target.result, function(oImg) {
+          oImg.width = chart.width;
+          oImg.height = chart.height;
+          chart.add(oImg);
+        });
+      };
+      reader.readAsDataURL(f);
+    }
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+
+  fileInput = document.getElementById('file-input');
+  fileInput.addEventListener('change', handleFileSelect, false);
+
+  openFile = document.getElementById('open-file');
+  openFile.addEventListener('click', function(e) {
+    if (fileInput) {
+      fileInput.click();
+    }
+    e.preventDefault();
+  }, false);
 });
